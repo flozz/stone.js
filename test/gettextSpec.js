775d283b77782c2d5b93ae34ee38b7cda69040f2
Thomas Fromont <tfromont@wanadev.fr>
2022-09-30 16:03:49 +0200

    reorder tests

--- test/gettextSpec.js
+++ test/gettextSpec.js
@@ -126,6 +126,14 @@ describe("gettext", function () {
 
     });
 
+    describe("gettext_noop", function () {
+
+        it("doest not operate translation", function () {
+            var someString = "Some string to translate later";
+            expect(StoneTest.index.gettext_noop(someString)).toEqual(someString);
+        });
+    });
+
     describe("gettext", function () {
 
         describe("With legacy catalog format", function () {
@@ -197,6 +205,62 @@ describe("gettext", function () {
                     .toEqual("Heureux de vous revoir, John!");
             });
         });
+    });
+
+    describe("lazyGettext", function () {
+
+        it("can translate strings", function () {
+            var s = StoneTest.index.lazyGettext("Hello World");
+            StoneTest.index.setLocale(null);
+            expect(String(s)).toEqual("Hello World");
+            StoneTest.index.setLocale("fr");
+            expect(String(s)).toEqual("Bonjour le monde");
+        });
+
+        it("can translate strings with replacements", function () {
+            var s = StoneTest.index.lazyGettext("Hello {name}", {name: "John"});
+            StoneTest.index.setLocale(null);
+            expect(String(s)).toEqual("Hello John");
+            StoneTest.index.setLocale("fr");
+            expect(String(s)).toEqual("Bonjour John");
+        });
+
+    });
+
+    describe("LazyString", function () {
+
+        beforeAll(function () {
+            StoneTest.index.setLocale("fr");
+        });
+
+        beforeEach(function () {
+            this.lazy = new StoneTest.index.LazyString("Hello World");
+        });
+
+        it("can translate text", function () {
+            expect(this.lazy.toString()).toEqual("Bonjour le monde");
+        });
+
+        it("can mimic the String API", function () {
+            var stringProps = Object.getOwnPropertyNames(String.prototype);
+            var lazyProps = Object.getOwnPropertyNames(this.lazy);
+            for (var i = 0 ; i < stringProps.length ; i++) {
+                expect(lazyProps).toContain(stringProps[i]);
+            }
+        });
+
+        it("can give the translated string length", function () {
+            expect(this.lazy.length).toEqual(16);
+        });
+
+        it("can return the translated string in lowerCase", function () {
+            expect(this.lazy.toLowerCase()).toEqual("bonjour le monde");
+        });
+
+        it("can return a splitted translated string", function () {
+            expect(this.lazy.split(" ")).toEqual(["Bonjour", "le", "monde"]);
+        });
+
     });
 
     describe("ngettext", function () {
@@ -298,70 +362,6 @@ describe("gettext", function () {
                 expect(StoneTest.index.ngettext("car", "cars", 2)).toEqual("macchine");
             });
         });
-    });
-
-    describe("lazyGettext", function () {
-
-        it("can translate strings", function () {
-            var s = StoneTest.index.lazyGettext("Hello World");
-            StoneTest.index.setLocale(null);
-            expect(String(s)).toEqual("Hello World");
-            StoneTest.index.setLocale("fr");
-            expect(String(s)).toEqual("Bonjour le monde");
-        });
-
-        it("can translate strings with replacements", function () {
-            var s = StoneTest.index.lazyGettext("Hello {name}", {name: "John"});
-            StoneTest.index.setLocale(null);
-            expect(String(s)).toEqual("Hello John");
-            StoneTest.index.setLocale("fr");
-            expect(String(s)).toEqual("Bonjour John");
-        });
-
-    });
-
-    describe("gettext_noop", function () {
-
-        it("doest not operate translation", function () {
-            var someString = "Some string to translate later";
-            expect(StoneTest.index.gettext_noop(someString)).toEqual(someString);
-        });
-    });
-
-    describe("LazyString", function () {
-
-        beforeAll(function () {
-            StoneTest.index.setLocale("fr");
-        });
-
-        beforeEach(function () {
-            this.lazy = new StoneTest.index.LazyString("Hello World");
-        });
-
-        it("can translate text", function () {
-            expect(this.lazy.toString()).toEqual("Bonjour le monde");
-        });
-
-        it("can mimic the String API", function () {
-            var stringProps = Object.getOwnPropertyNames(String.prototype);
-            var lazyProps = Object.getOwnPropertyNames(this.lazy);
-            for (var i = 0 ; i < stringProps.length ; i++) {
-                expect(lazyProps).toContain(stringProps[i]);
-            }
-        });
-
-        it("can give the translated string length", function () {
-            expect(this.lazy.length).toEqual(16);
-        });
-
-        it("can return the translated string in lowerCase", function () {
-            expect(this.lazy.toLowerCase()).toEqual("bonjour le monde");
-        });
-
-        it("can return a splitted translated string", function () {
-            expect(this.lazy.split(" ")).toEqual(["Bonjour", "le", "monde"]);
-        });
-
     });
 
     describe("LazyNString", function () {
